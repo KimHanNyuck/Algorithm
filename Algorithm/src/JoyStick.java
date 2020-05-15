10f43ab09ac70edde0107d6c7339de49ccaa3182
KimHanNyuck <revubplay@nate.com>
2020-05-15 16:58:58 +0900

    JoyStick

---
+++ Algorithm/src/JoyStick.java
@@ -0,0 +1,33 @@
+public class JoyStick {
+	
+	public int solution(String name) {
+        int answer = 0;
+        int exp = name.length() - 1;
+        for(int i = 0; i < name.length(); i++){
+            char c = name.charAt(i);
+            answer += ('Z'- c + 1) > c - 'A' ? c - 'A' : ('Z' - c + 1);
+            if(c == 'A'){
+                int nextIdx = i+1;
+                int countA = 0;
+                while (nextIdx < name.length() &&
+                       name.charAt(nextIdx) == 'A'){
+                    countA ++;
+                    nextIdx++;
+                }
+                int tmp = (i-1)*2 + (name.length() -1 -i - countA) ;
+                if(exp > tmp) exp = tmp;
+            }
+        }
+
+        answer += exp;
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JoyStick js = new JoyStick();
+		int sol = js.solution("AAABAAA");
+		System.out.println(sol);
+	}
+
+}
