726f168fc9f2341b6fed4b97348eec48c4d92521
KimHanNyuck <revubplay@nate.com>
2020-06-16 15:55:58 +0900

    Longest_Ndoes

--- Algorithm/src/Longest_Node.java
+++ Algorithm/src/Longest_Node.java
@@ -1,34 +1,57 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
 public class Longest_Node {
 	public int solution(int n, int[][] edge) {
         int answer = 0;
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        Map<Integer, Integer> level = new HashMap<Integer, Integer>();
+        Map<Integer, ArrayList> map = new HashMap<Integer, ArrayList>();
+        Map<Integer, ArrayList> level = new HashMap<Integer, ArrayList>();
         for(int i = 0 ; i < edge.length; i++) {
         	if(edge[i][0] > edge[i][1]) {
-        		map.put(edge[i][1],edge[i][0]);
+        		if(!map.containsKey(edge[i][1])) {
+        			ArrayList list = new ArrayList();
+        			list.add(edge[i][0]);
+        			map.put(edge[i][1],list);
+        		} else {
+        			map.get(edge[i][1]).add(edge[i][0]);
+        		
+        		}
         	} else {
-        		map.put(edge[i][0],edge[i][1]);
+        		if(!map.containsKey(edge[i][0])) {
+        			ArrayList list = new ArrayList();
+        			list.add(edge[i][1]);
+        			map.put(edge[i][0],list);
+        		} else {
+        			map.get(edge[i][0]).add(edge[i][1]);
+        		}
         	}
         }
-        Iterator<Entry<Integer, Integer>> set = map.entrySet().iterator();
-        if(set.hasNext()) {
-        	
+        Iterator<Integer> it = map.keySet().iterator();
+        int stairs = 1;
+        while(it.hasNext()) {
+        	for(int i = 0 ; i < map.get(it.next()).size(); i++){
+        		if(!level.containsKey(stairs)) {
+        			ArrayList llist = new ArrayList<>();
+            		llist.add(map.get(it.next()).get(i));
+            		level.put(stairs, llist);
+        		} else {
+        			level.get(stairs).add(map.get(it.next()).get(i));
+        		}
+        		
+        	}
+        	stairs++;
         }
-        
+        answer = level.get(level.size()).size();
         
         
         return answer;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Longest_Node ln = new Longest_Node();
+		ln.solution(6, new int[][] {{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}});
 	}
 
 }
