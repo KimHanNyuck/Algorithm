ce9ebea829dd7f1075f435cde80c3e93beac6dfa
KimHanNyuck <revubplay@nate.com>
2020-08-16 04:47:51 +0900

    changes

--- Algorithm/src/Camouflage.java
+++ Algorithm/src/Camouflage.java
@@ -1,28 +1,24 @@
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class Camouflage {
 	public int solution(String[][] clothes) {
-        int answer = 1;
-        Map<String, Integer> map = new HashMap<>();
-        for(int i = 0 ; i < clothes.length ; i ++) {
-        	if(map.get(clothes[i][1])==null) {
-        		map.put(clothes[i][1], 1);
-        	} else {
-        		map.replace(clothes[i][1], map.get(clothes[i][1])+1);
-        	}
-        }
-        for(Map.Entry<String, Integer> elem : map.entrySet()) {
-        	if(map.size() == 1) {
-        		return elem.getValue();
-        	} else {
-        		answer *= elem.getValue();
-        	}
-        }
-        answer += clothes.length;
-        return answer;
-    }
+		int answer = 0;
+		Map<String, Integer> map = new HashMap<>();
+		for (int i = 0; i < clothes.length; i++) {
+			if (map.get(clothes[i][1]) == null) {
+				map.put(clothes[i][1], 1);
+			} else {
+				map.replace(clothes[i][1], map.get(clothes[i][1]) + 1);
+			}
+		}
+		for (Map.Entry<String, Integer> elem : map.entrySet()) {
+			answer *= (elem.getValue() + 1);
+		}
+		answer -= 1;
+		return answer;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
