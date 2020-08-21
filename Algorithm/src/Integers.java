042c3e6b7b0e463548189bb6038d2ba56eeecfc3
KimHanNyuck <revubplay@nate.com>
2020-08-22 03:53:12 +0900

    Words to Numbers

---
+++ Algorithm/src/Integers.java
@@ -0,0 +1,11 @@
+
+public class Integers {
+	public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
