15df5fe756efad5c8c58dc0f1873b7afedbe7fcc
KimHanNyuck <revubplay@nate.com>
2020-08-23 10:12:02 +0900

    Reverse_Arrays

---
+++ Algorithm/src/Reverse_Array.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Reverse_Array {
+	public int[] solution(long n) { 
+        return Arrays.stream(new StringBuilder(String.valueOf(n)).reverse().toString().split("")).mapToInt(i->Integer.parseInt(i)).toArray();
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
