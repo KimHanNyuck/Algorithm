3d5abf9ae5e8ee92d89fcb22669fd8b2b20bbc9d
KimHanNyuck <revubplay@nate.com>
2020-08-23 11:28:51 +0900

    Sqrt

---
+++ Algorithm/src/Square_Root.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Square_Root {
+	public long solution(long n) {
+        return Math.sqrt(n)==(long)Math.sqrt(n) ? (long)Math.sqrt(n) : -1 ;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println((long)Math.sqrt(14)== Math.sqrt(14));
+	}
+
+}
