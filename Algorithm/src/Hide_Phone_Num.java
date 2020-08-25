993ab11358e0f25fba9821874b4a9e0041a247e9
KimHanNyuck <revubplay@nate.com>
2020-08-25 11:53:03 +0900

    Hide_Phone_Num

---
+++ Algorithm/src/Hide_Phone_Num.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Hide_Phone_Num {
+	public String solution(String phone_number) {
+		char[] c = phone_number.toCharArray();
+		String[] s = new String[phone_number.length()];
+		for (int i = 0; i < phone_number.length() - 4; i++) {
+			if(i < phone_number.length() -4) {
+				c[i] = '*';
+			}
+			s[i] = String.valueOf(c[i]);
+		}
+		return Arrays.stream(s).reduce((a,b) -> a+b).orElse(other);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
