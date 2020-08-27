c57df4a58e2ba5f77519105eb4b1d97d1df526fe
KimHanNyuck <revubplay@nate.com>
2020-08-27 16:16:07 +0900

    Biggest_Number

--- Algorithm/src/Make_Biggest_Number.java
+++ Algorithm/src/Make_Biggest_Number.java
@@ -1,45 +1,26 @@
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class Make_Biggest_Number {
-	
+
 	public String solution(String number, int k) {
-        String answer = "";
-        ArrayList list = new ArrayList<>();
-        ArrayList removelist = new ArrayList<>();
-        ArrayList answerlist = new ArrayList<>();
-        for(int i = 0 ; i < number.length() ; i ++) {
-        	list.add(number.charAt(i));
-        	answerlist.add(number.charAt(i));
-        }
-        Collections.sort(list);
-        for(int i = 1 ; i <= k ; i ++) {
-            removelist.add(list.get(0));
-        	list.remove(0);
-        }
-        for(int i = 0 ; i < answerlist.size() -1 ; i ++) {
-        	System.out.println(answerlist.get(i));
-        }
-        for(int i = 0 ; i < answerlist.size() -1 ; i ++) {
-        	if(answerlist.get(i).equals((removelist.get(0)))) {
-        		answerlist.remove(removelist.get(0));
-        		removelist.remove(0);
-        	} else {
-        		continue;
-        	}
-        }
-        for(int i = list.size()-1 ; i >= 0 ; i --) {
-        	answer += list.get(i);
-        }
-        
-        System.out.println("..........................");
-        for(int i = 0 ; i < removelist.size() -1 ; i ++){
-        	System.out.println(removelist.get(i));
-        }
-        
-        return answer;
-    }
-	
+		int idx = 0;
+		StringBuilder answer = new StringBuilder();
+
+		if (number.charAt(0) == '0') {
+			return "0";
+		}
+		for (int i = 0; i < number.length() - k; i++) {
+			char c = '0';
+			for (int j = idx; j <= k + i; j++) {
+				if (c < number.charAt(j)) {
+					c = number.charAt(j);
+					idx = j + 1;
+				}
+			}
+			answer.append(c);
+		}
+		return answer.toString();
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Make_Biggest_Number mbn = new Make_Biggest_Number();
