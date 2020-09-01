c49bda039a7a16c520bf5cdbdc81d230878a949d
KimHanNyuck <revubplay@nate.com>
2020-09-01 17:06:13 +0900

    n진수

--- Algorithm/src/Country_124.java
+++ Algorithm/src/Country_124.java
@@ -2,16 +2,22 @@
 public class Country_124 {
 	public String solution(int n) {
         String answer = "";
-        for(int i = 1 ; i <= n ; i++) {
-        	if(i%3 = 1) {
-        		
-        	}
+        StringBuffer sb = new StringBuffer();
+        while(n > 0) {
+        	sb.insert(0, n%3);
+        	n /= 3;
         }
+        answer = sb.toString();
+        System.out.println(sb);
+        answer.replaceAll("0", "1").replaceAll("2", "4");
+        System.out.println(answer);
+        
         return answer;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Country_124 c1 = new Country_124();
+		c1.solution(8);
 	}
 
 }
