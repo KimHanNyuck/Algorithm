7e49e895169a1bbb802eb0880c60b6207ea63d39
KimHanNyuck <revubplay@nate.com>
2020-09-01 18:11:10 +0900

    fix

--- Algorithm/src/Country_124.java
+++ Algorithm/src/Country_124.java
@@ -4,20 +4,16 @@ public class Country_124 {
         String answer = "";
         StringBuffer sb = new StringBuffer();
         while(n > 0) {
-        	sb.insert(0, n%3);
-        	n /= 3;
+            sb.insert(0, n%3);
+            n /= 3;
         }
-        answer = sb.toString();
         System.out.println(sb);
-        answer.replaceAll("0", "1").replaceAll("2", "4");
-        System.out.println(answer);
-        
         return answer;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Country_124 c1 = new Country_124();
-		c1.solution(8);
+		c1.solution(12);
 	}
 
 }
