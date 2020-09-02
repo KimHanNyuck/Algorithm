d457f597e3585e37f7758cbc032d792a5eb3899c
KimHanNyuck <revubplay@nate.com>
2020-09-02 22:57:54 +0900

    124_country

--- Algorithm/src/Country_124.java
+++ Algorithm/src/Country_124.java
@@ -2,18 +2,18 @@
 public class Country_124 {
 	public String solution(int n) {
         String answer = "";
-        StringBuffer sb = new StringBuffer();
-        while(n > 0) {
-            sb.insert(0, n%3);
-            n /= 3;
+        String[] num = {"4","1","2"};
+        while(n>0) {
+            answer = num[n%3]+answer;
+            n = (n-1)/3;
         }
-        System.out.println(sb);
         return answer;
     }
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Country_124 c1 = new Country_124();
-		c1.solution(12);
+		c1.solution(1);
 	}
 
 }
