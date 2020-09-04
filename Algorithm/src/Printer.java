ba750b0da56de40e7e5ca6f4bcd063676f191406
KimHanNyuck <revubplay@nate.com>
2020-09-04 23:08:41 +0900

    Printer

--- Algorithm/src/Printer.java
+++ Algorithm/src/Printer.java
@@ -1,16 +1,24 @@
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Printer {
 	public int solution(int[] priorities, int location) {
-        int answer = 0;
-        Queue<Integer> q = new LinkedList<Integer>();
-        Arrays.stream(priorities).forEach(i -> q.add(i));
-        while(!q.isEmpty()) {
-        	int max = q.stream().reduce(0, Integer::max).intValue();
-        	
+        int answer = 1;
+        PriorityQueue<Integer> prior = new PriorityQueue<Integer>(Collections.reverseOrder());
+        Arrays.stream(priorities).forEach(i -> {prior.add(i)});
+        while(!prior.isEmpty()){
+            for(int i=0; i<priorities.length; i++){
+                if(priorities[i] == (int)prior.peek()) {
+                    if(i == location){
+                        return answer;
+                    }
+                    prior.poll();
+                    answer++;
+                }
+            }
         }
+
         return answer;
     }
 	public static void main(String[] args) {
