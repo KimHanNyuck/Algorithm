56c2c38f07d5cc7d8a2fe16c66274b6520bedf3d
KimHanNyuck <revubplay@nate.com>
2020-09-15 14:13:51 +0900

    JadenCase

--- Algorithm/src/Arrays_zip.java
+++ Algorithm/src/Arrays_zip.java
@@ -1,6 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Arrays_zip {
-
+	public int solution(String s) {
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap();
+        for(int i = 1 ; i <= s.length()/2; i ++) {
+        	if(s.length()%i==0) {
+        		map.put(i, 0);
+        	}
+        }
+        return answer;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- Algorithm/src/JadenCase.java
+++ Algorithm/src/JadenCase.java
@@ -5,13 +5,22 @@ public class JadenCase {
         String answer = "";
         String[] list = s.split(" ");
         for(int i = 0 ; i < list.length ; i ++) {
-        	System.out.println(list[i]);
         	if(list[i].equals(" ")) {
         		answer += " ";
         		continue;
         	}
+        	try {
         	if(list[i].charAt(0)>= 97 && list[i].charAt(0) <= 122) {
-        		list[i] = String.valueOf(list[i].charAt(0)).toUpperCase()+list[i].substring(1).toLowerCase();
+        		if(list[i].length()==1) {
+        			list[i] = String.valueOf(list[i].charAt(0)).toUpperCase();
+        		} else {
+        			list[i] = String.valueOf(list[i].charAt(0)).toUpperCase()+list[i].substring(1).toLowerCase();
+        		}
+        	} else {
+        		list[i] = list[i];
+        	}
+        	} catch(Exception e) {
+        		list[i] = "";
         	}
         	if(i == list.length-1) {
         		answer += list[i];
@@ -19,13 +28,22 @@ public class JadenCase {
         		answer += list[i] + " ";
         	}
         }
+        if (answer.length() != s.length()) { 
+			int length = s.length() - answer.length();
+			for (int i = 0; i < length; i++) {
+				answer += " ";
+			}
+		}
         System.out.println(answer);
         return answer;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JadenCase jc = new JadenCase();
-		jc.solution("\"Hello My      Friend\"");
+		jc.solution("  for the last week  ");
+		jc.solution("  For The Last W eek");
+		System.out.println("s                 ");
+		
 	}
 
 }
