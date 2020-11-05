12e80d1b645c2f0f26799059e2483ceba768405b
KimHanNyuck <revubplay@nate.com>
2020-11-05 13:48:45 +0900

    bt

---
+++ Algorithm/src/Bridge_Truck.java
@@ -0,0 +1,13 @@
+
+public class Bridge_Truck {
+	public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Algorithm/src/plus.java
+++ Algorithm/src/plus.java
@@ -1,20 +1,19 @@
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class plus {
-	public int[] solution(int[] numbers) {
-        int[] answer = {};
-        Set set = new HashSet<Integer>();
-        for(int i = 0 ; i < numbers.length - 1 ; i++) {
-        	for(int j = i+1 ; j < numbers.length; j++) {
-        		set.add(numbers[i]+numbers[j]);
-        	}
-        }
-        Stream<String> stream = set.stream();
-        return stream.mapToInt(Integer::valueOf).toArray();
-    }
+		public int[] solution(int[] numbers) {
+	        int[] answer = {};
+	        Set set = new HashSet<Integer>();
+	        for(int i = 0 ; i < numbers.length - 1 ; i++) {
+	        	for(int j = i+1 ; j < numbers.length; j++) {
+	        		set.add(numbers[i]+numbers[j]);
+	        	}
+	        }
+	        Stream<Integer> stream = set.stream().sorted();
+	        return stream.mapToInt(i->i).toArray();
+	    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
